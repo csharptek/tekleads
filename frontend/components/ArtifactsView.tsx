08372b6337518cad75686d08da4b0b900756a8f9
Csharptek <info@csharptek.com>
2026-05-21 20:46:03 +0530

    follow2

--- frontend/components/ArtifactsView.tsx
+++ frontend/components/ArtifactsView.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { api } from "../../lib/api";
+import { api } from "../lib/api";
 
 type Artifacts = {
   coverLetter?: string;
